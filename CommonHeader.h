9b280ee9acb2c53d3807a76c5b7ec9aa2dd29d31
bluedefelisa <77630669+bluedefelisa@users.noreply.github.com>
2021-03-31 17:16:43 +0700

    Add files via upload

---
+++ CommonHeader.h
@@ -0,0 +1,29 @@
+#ifndef CommonHeader_H_
+
+#define CommonHeader_H_
+
+#include<iostream>
+#include<SDL.h>
+#include <SDL_ttf.h>
+#include<SDL_image.h>
+#include<string>
+
+using namespace std;
+
+const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 1280;
+
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
+
+
+#endif // CommonHeader_H_
+
+
+
+
+
+
+
+
+
